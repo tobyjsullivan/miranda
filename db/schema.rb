c36a8b5e421ae93839d04dd165b193f3eca01f73
Toby Sullivan <tobyjsullivan@gmail.com>
2019-05-05 21:03:39 +1000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_081401) do
+ActiveRecord::Schema.define(version: 2019_05_05_090053) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_boards_on_slug"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
