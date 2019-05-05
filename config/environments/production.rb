46dec3c0ac969a43b212f6e6150f2a920a601497
Toby Sullivan <tobyjsullivan@gmail.com>
2019-05-05 18:56:09 +1000

    Add devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # TODO (toby): Update host to proper domain when there is one.
+  config.action_mailer.default_url_options = { host: 'example.com', port: 80 }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
